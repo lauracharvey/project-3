db3c87054ed53783483dffc5aafa56ca5d0d494f
lauracharvey <lauracarolharvey@gmail.com>
2020-11-12 14:20:05 +0000

    sixth commit

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 const express = require('express')
 const expressServer = express()
-const { port } = require('./config/environment')
 require('dotenv').config()
 const Router = require('./router')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-
-
 mongoose.connect(
   'mongodb://localhost/sortDB',
   { userNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
   (err) => {
-
     if (err) console.log(err)
     else console.log('Mongoose connected successfully!')
-
   }
 )
 
