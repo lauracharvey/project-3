ee917b2c19fb6bd4cc1b01b916005106d266fddc
lauracharvey <lauracarolharvey@gmail.com>
2020-11-23 10:46:35 +0000

    footer changes

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -3,8 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return <footer>
-    <small>Made with ❤️ by 
-    <Link to={'https://www.linkedin.com/in/csilla-barna-2838b11b6/'}>Csilla Barna</Link>, <Link to={'https://www.lauracharvey.dev/'}>LauraCHarvey</Link> & <Link to={'https://www.linkedin.com/in/sherryll-elliott-b41b6b15a/'}>Sherryll Elliott</Link></small>
+    <small>Made with ❤️ by <Link to={'https://www.linkedin.com/in/csilla-barna-2838b11b6/'}>Csilla Barna</Link>, <Link to={'https://www.lauracharvey.dev/'}>LauraCHarvey</Link> & <Link to={'https://www.linkedin.com/in/sherryll-elliott-b41b6b15a/'}>Sherryll Elliott</Link></small>
   </footer>
 }
 
